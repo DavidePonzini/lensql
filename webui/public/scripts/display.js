4d2d84218f0021d62508b8df42c3ff2c7640b942
Davide Ponzini <davide.ponzini95@gmail.com>
2025-04-07 12:18:41 +0200

    display list*

---
+++ webui/public/scripts/display.js
@@ -0,0 +1,55 @@
+function display_table(json_data) {
+    let data = JSON.parse(json_data);
+
+    let columns = data['columns'];
+    let row_idx = data['index'];
+    let rows = data['data'];
+
+    let result = $('#result');
+    result.empty();
+
+    let table = $('<table></table>').addClass('table table-striped table-bordered');
+    let thead = $('<thead></thead>');
+    let header_row = $('<tr></tr>');
+    let header = $('<th></th>').text('Index');
+    header_row.append(header);
+
+    for (let i = 0; i < columns.length; i++) {
+        let th = $('<th></th>').text(columns[i]);
+        header_row.append(th);
+    }
+    thead.append(header_row);
+    table.append(thead);
+
+    let tbody = $('<tbody></tbody>');
+    for (let i = 0; i < rows.length; i++) {
+        let row = $('<tr></tr>');
+        let index_cell = $('<td></td>').text(row_idx[i]);
+        row.append(index_cell);
+
+        for (let j = 0; j < rows[i].length; j++) {
+            let cell = $('<td></td>').text(rows[i][j]);
+            row.append(cell);
+        }
+        tbody.append(row);
+    }
+
+    table.append(tbody);
+    result.append(table);
+}
+
+function display_text(json_text) {
+    let text = JSON.parse(json_text);
+
+    let result = $('#result');
+    result.empty();
+
+    let pre = $('<pre></pre>').text(text);
+    result.append(pre);
+}
+
+function clear_result() {
+    let result = $('#result');
+    result.empty();
+}
+
