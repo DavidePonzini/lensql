6fd169be2927bed371ddd5fd19d18c2a6785d85d
Davide Ponzini <davide.ponzini95@gmail.com>
2025-04-14 11:33:37 +0200

    Quick commit (2025-04-14)

--- database/11-analytical_views.sql
+++ database/11-analytical_views.sql
@@ -2,12 +2,25 @@ BEGIN;
 
 SET search_path to lensql;
 
+CREATE OR REPLACE VIEW v_queries AS
+SELECT 
+    count(*) AS total_queries,
+    count(DISTINCT query) AS unique_queries,
+    count(DISTINCT username) AS unique_users
+FROM
+    queries
+WHERE
+    username LIKE 's%';
+
 CREATE OR REPLACE VIEW v_button_presses AS
 SELECT 
     button,
     COUNT(*) AS press_count
 FROM 
     messages
+    JOIN queries ON messages.query_id = queries.id
+WHERE
+    username LIKE 's%'
 GROUP BY 
     button
 ORDER BY 
@@ -21,6 +34,8 @@ SELECT
 FROM 
     messages
     JOIN queries ON messages.query_id = queries.id
+WHERE
+    username LIKE 's%'
 GROUP BY 
     username, button
 ORDER BY 
@@ -29,10 +44,17 @@ ORDER BY
 CREATE OR REPLACE VIEW v_user_query_counts AS
 SELECT 
     username,
-    COUNT(*) FILTER (WHERE success) AS success_count,
-    COUNT(*) FILTER (WHERE NOT success) AS error_count
+    COUNT(*) FILTER (WHERE success) AS success,
+    COUNT(DISTINCT query) FILTER (WHERE success) AS unique_success,
+    COUNT(messages.id) FILTER (WHERE success) AS success_message,
+    COUNT(*) FILTER (WHERE NOT success) AS error,
+    COUNT(DISTINCT query) FILTER (WHERE NOT success) AS unique_error,
+    COUNT(messages.id) FILTER (WHERE NOT success) AS error_message
 FROM 
     queries
+    LEFT JOIN messages ON queries.id = messages.query_id
+WHERE
+    username LIKE 's%'
 GROUP BY 
     username
 ORDER BY 
@@ -46,6 +68,9 @@ SELECT
     COUNT(*) FILTER (WHERE feedback = FALSE) AS negative_feedback
 FROM
     messages
+    JOIN queries ON messages.query_id = queries.id
+WHERE
+    username LIKE 's%'
 GROUP BY
     button
 ORDER BY
