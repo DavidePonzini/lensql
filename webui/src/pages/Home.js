2d8599e0bf311d50a8f05395fb3515a40877cae9
Davide Ponzini <davide.ponzini95@gmail.com>
2025-05-20 16:40:20 +0200

    formatting

--- webui/src/pages/Home.js
+++ webui/src/pages/Home.js
@@ -4,7 +4,9 @@ function Home() {
             <h1>Welcome to <b>LensQL</b></h1>
             <p>This page is under construction.</p>
 
-            <a href="/assignments/q/5">Click here to access the editor</a>
+            <a href="/assignments/q/5">
+                &gt;&gt; Click here to access the editor &lt;&lt;
+            </a>
         </div>
     );
 }
