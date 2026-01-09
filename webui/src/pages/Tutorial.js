773c02d58447cbed2121adfbd7967983635d8887
Davide Ponzini <davide.ponzini95@gmail.com>
2026-01-09 16:26:46 +0100

    created dedicated tutorial page

---
+++ webui/src/pages/Tutorial.js
@@ -0,0 +1,12 @@
+import TutorialCarousel from '../components/Tutorial';
+
+
+function Tutorial() {
+    return (
+        <div className="container px-5 py-5 bg-light">
+            <TutorialCarousel />
+        </div>
+    );
+}
+
+export default Tutorial;
