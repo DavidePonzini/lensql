94c325632a1b719fc39db9c6c0c89bba4fe0799b
Davide Ponzini <davide.ponzini95@gmail.com>
2026-02-26 16:14:01 +0100

    dataset progress view

--- db_admin/setup/02_views_datasets.sql
+++ db_admin/setup/02_views_datasets.sql
@@ -61,4 +61,57 @@ WHERE dm.is_active = TRUE
 
 GROUP BY dm.username, d.id, d.name, d.description, dm.is_owner, dm.joined_ts;
 
+CREATE VIEW v_generated_exercises AS
+SELECT
+    e.dataset_id,
+    e.generation_error,
+    ARRAY_AGG(e.generation_difficulty ORDER BY e.generation_difficulty) AS generation_difficulties
+FROM exercises e
+GROUP BY e.dataset_id, e.generation_error
+ORDER BY e.dataset_id, e.generation_error;
+
+CREATE VIEW v_dataset_completion AS
+WITH solved_exercises AS (
+    SELECT
+        qb.username,
+        e.id AS exercise_id
+    FROM exercises e
+    JOIN query_batches qb ON qb.exercise_id = e.id
+    JOIN queries q ON q.batch_id = qb.id
+    JOIN exercise_solutions es ON es.id = q.id
+    WHERE es.is_correct = TRUE
+    GROUP BY e.id, qb.username
+), dataset_progress AS (
+    SELECT
+        dm.username,
+        d.id AS dataset_id,
+        COUNT(DISTINCT e.id) AS total_exercises,
+        COUNT(DISTINCT se.exercise_id) AS solved_exercises
+    FROM datasets d
+    JOIN dataset_members dm ON dm.dataset_id = d.id
+    JOIN exercises e ON e.dataset_id = d.id AND e.is_hidden = FALSE
+    LEFT JOIN solved_exercises se ON se.exercise_id = e.id AND se.username = dm.username
+    WHERE
+        dm.is_active = TRUE
+        AND NOT dm.is_owner
+    GROUP BY dm.username, d.id
+)
+
+SELECT
+    username,
+    dataset_id,
+    solved_exercises AS solved,
+    total_exercises - solved_exercises AS remaining,
+    ((solved_exercises::DECIMAL / total_exercises) * 100)::DECIMAL(5,2) AS completion
+FROM dataset_progress
+WHERE
+    total_exercises > 0
+    AND solved_exercises > 0
+ORDER BY
+    completion DESC,
+    dataset_id,
+    username;
+
+
+
 COMMIT;
