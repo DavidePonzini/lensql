57ef75f152e77cfb23da20767513c3c54de0886d
Davide Ponzini <davide.ponzini95@gmail.com>
2026-04-21 10:05:47 +0200

    removed unused import

--- server/db/users/__init__.py
+++ server/db/users/__init__.py
@@ -2,8 +2,6 @@ from .database import Database
 from .postgresql import PostgresqlDatabase
 from .mysql import MySQLDatabase
 
-import os
-
 
 def get_database(dbname: str, dbms: str) -> Database:
     '''Factory function to get the appropriate database backend.'''
